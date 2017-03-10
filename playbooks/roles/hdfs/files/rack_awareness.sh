87323ab7b6b10a3047e69a80bed347bbd50dcebf
Jeff Wang <jeffwji@gmail.com>
2017-03-10 16:30:26 -0500

    add config

---
+++ playbooks/roles/hdfs/files/rack_awareness.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+HADOOP_CONF=/opt/hdfs/default/etc/hadoop
+
+while [ $# -gt 0 ] ; do
+  nodeArg=$1
+  exec< ${HADOOP_CONF}/rack_awareness.topology
+  result=""
+  while read line ; do
+    ar=( $line )
+    if [ "${ar[0]}" = "$nodeArg" ] ; then
+      result="${ar[1]}"
+    fi
+  done
+  shift
+  if [ -z "$result" ] ; then
+    echo -n "/default/rack"
+  else
+    echo "$result"
+  fi
+done
+
